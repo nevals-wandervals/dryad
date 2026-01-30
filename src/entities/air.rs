3f17d18a5b387d7319e49a6b0bd471c27b528981
Nevals Wandervals <nevals.wandervals@yandex.ru>
2026-01-30 19:41:02 +0300

    impl `properties::` for `Air`

--- src/entities/air.rs
+++ src/entities/air.rs
@@ -1,2 +1,68 @@
+use crate::{entities::traits::properties::PhysicalProperties, math::units::{Mass, Pressure, Temperature, Volume}};
+
+use super::traits::properties;
+
+
 #[derive(Debug)]
-pub struct Air {}
+pub struct Air {
+    temperature: Temperature,
+    pressure: Pressure,
+    mass: Mass,
+    volume: Volume,
+    water_fraction: f32,
+    gas_fraction: f32,
+}
+
+impl properties::PhysicalProperties for Air {
+    fn temperature(&self) -> crate::math::units::Temperature {
+        self.temperature
+    }
+
+    fn pressure(&self) -> crate::math::units::Pressure {
+        self.pressure
+    }
+
+    fn mass(&self) -> Mass {
+        self.mass
+    }
+
+    fn volume(&self) -> crate::math::units::Volume {
+        self.volume
+    }
+
+    fn get_mut_temperature(&mut self) -> &mut crate::math::units::Temperature {
+        &mut self.temperature
+    }
+
+    fn get_mut_pressure(&mut self) -> &mut crate::math::units::Pressure {
+        &mut self.pressure
+    }
+
+    fn get_mut_mass(&mut self) -> &mut Mass {
+        &mut self.mass
+    }
+
+    fn get_mut_volume(&mut self) -> &mut crate::math::units::Volume {
+        &mut self.volume
+    }
+}
+
+impl properties::ChemicalProperties for Air { }
+
+impl properties::Composition for Air {
+    fn water_fraction(&self) -> f32 {
+        self.water_fraction
+    }
+
+    fn solid_fraction(&self) -> f32 {
+        0.0
+    }
+
+    fn gas_fraction(&self) -> f32 {
+        self.gas_fraction
+    }
+
+    fn total_mass(&self) -> crate::math::units::Mass {
+        self.mass()
+    }
+}
